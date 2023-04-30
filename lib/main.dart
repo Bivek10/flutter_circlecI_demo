dbfe852a3e0c1bdab55f87d031b4f110955e61d3
= <bibekkarki662@gmail.com>
2023-05-01 00:16:13 +0545

    config changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_circleci_demo/firebase_options.dart';
 
 void main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
